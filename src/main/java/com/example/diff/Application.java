70351cdd40f61feb0bcd087ec6f1227ffecdf35b
姜峰 <harrison.jiang@corp.netease.com>
2021-12-30 13:35:47 +0800

    变更

--- src/main/java/com/example/diff/Application.java
+++ src/main/java/com/example/diff/Application.java
@@ -3,8 +3,11 @@ package com.example.diff;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
 
 @SpringBootApplication
+@ServletComponentScan
 @MapperScan("com.example.diff.mapper")
 public class Application {
 
--- src/main/java/com/example/diff/filter/LoginFilter.java
+++ src/main/java/com/example/diff/filter/LoginFilter.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebFilter(urlPatterns = "/api/**", filterName = "loginFilter")
+@WebFilter(urlPatterns = "/*", filterName = "loginFilter")
 public class LoginFilter implements Filter {
 
 
